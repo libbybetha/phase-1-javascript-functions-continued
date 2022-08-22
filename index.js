df82b839f126ef4561a707391f628db2d6683500
Beth Njuguna <libbybetha@gmail.com>
2022-08-22 09:10:38 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate'){
+    return (`This Saturday, I want to ${activity}!`);
+    
+}
+console.log(saturdayFun('bathe my dog'))
+
+function mondayWork(activity = 'go to the office'){
+    return (`This Monday, I will ${activity}.`);
+    
+}
+console.log(mondayWork('work from home'));
+
+
+let wrapAdjective = function(style="*"){
+    return function(adjective="a hard worker"){
+        return (`You are ${style}${adjective}${style}!`);
+    }
+}
+
